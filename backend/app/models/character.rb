9ef4eac1ab8680e60617263cc0022db3f91041e4
Evelyn <evelyn.a.kosma@gmail.com>
2022-01-24 14:13:45 -0600

    attempted omniauth google log in, not currently functional

--- backend/app/models/character.rb
+++ backend/app/models/character.rb
@@ -1,4 +1,5 @@
 class Character < ApplicationRecord
   belongs_to :users
+  #scope :alphabeical, -> {order('name')}
   attributes :name, :race, :class, :alignment, :strength, :dexterity, :constitution, :intelligence, :wisdom, :charisma
 end
