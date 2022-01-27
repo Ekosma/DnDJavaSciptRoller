d22b1c32c0eae920308fb7b2ac63523f23d18846
Evelyn <evelyn.a.kosma@gmail.com>
2022-01-26 21:07:15 -0600

    rails s functional

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::API
+
+  def signup
+    User.new(params[:username], params[:password])
+  end
+
+  def login
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      render json: {user: user}
+    else
+      render json: {errors: "invalid username"}
+    end
+  end
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  resources :characters
-  resources :users
+
+  
+
+  post 'login', to: 'auth#login'
+  post 'signup', to: 'auth#signup'
+
+  
+  resources :characters do 
+    resources :users
+  end
+
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
